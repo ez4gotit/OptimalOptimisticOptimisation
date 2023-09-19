bf83d6bca3c2cb35a69e7b9c39466c2fd501375e
ez4gotit <mdudinov@gmail.com>
2023-09-19 19:20:16 +0600

    initial

---
+++ SimplexMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimplexMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
