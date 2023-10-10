ebce5a24cdb1adeba6ac1a74501b9441a86eade1
ez4gotit <mdudinov@gmail.com>
2023-10-11 01:47:31 +0600

    finally

--- SimplexMethod/Program.cs
+++ SimplexMethod/Program.cs
@@ -1,29 +1,32 @@
-﻿namespace SimplexMethod
+﻿using System; 
+
+namespace SimplexMethod
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Matrix C = new Matrix(new double[,] {{5, 4, 0, 0, 0, 0}});
-            // Matrix b = new Matrix(new double[,] {{24},{6},{1},{2}});
-            // Matrix A = new Matrix(new double[,] {{6, 4, 1, 0, 0, 0}, 
-            //                                      {1, 2, 0, 1, 0, 0},
-            //                                      {-1, 1, 0, 0, 1, 0},
-            //                                      {0, 1, 0, 0, 0, 1}});
-            
+            /*            Matrix C = new Matrix(new double[,] { { 5, 4, 0, 0, 0, 0 } });
+                        Matrix b = new Matrix(new double[,] { { 24 }, { 6 }, { 1 }, { 2 } });
+                        Matrix A = new Matrix(new double[,] {{6, 4, 1, 0, 0, 0},
+                                                              {1, 2, 0, 1, 0, 0},
+                                                              {-1, 1, 0, 0, 1, 0},
+                                                              {0, 1, 0, 0, 0, 1}});*/
+
 
             double[] arrayC = InputLibrary.ReadObjectiveFunctionCoefficients();
             int rows = arrayC.Length;
             double[,] arrayA = InputLibrary.ReadConstraintMatrix(rows);
+            rows = arrayA.GetLength(0);
             double[] arrayb = InputLibrary.ReadRightHandSide(rows);
             double accuracy = InputLibrary.ReadApproximationAccuracy();
             Matrix C = new Matrix(arrayC);
             Matrix A = new Matrix(arrayA);
-            Matrix b = new Matrix(arrayb);
+            Matrix b = new Matrix(arrayb).Transpose();
+/*            double accuracy = 0.01;*/
             (double z, Matrix vars) = SimplexAlgorithm.Optimize(C, A, b, accuracy);
             Console.WriteLine(vars.ToString());
-            Console.WriteLine(z);
-                            
+            Console.WriteLine(z);       
         }
     }
 }
